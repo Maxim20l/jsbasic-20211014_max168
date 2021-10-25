83b2b2a0bd134eaa5b4448393ac1f4443af52af4
Maxim Lyashenko <lyashenkokhpi2018@gmail.com>
2021-10-25 11:32:10 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,4 @@
-function checkSpam(str) {
-  // ваш код...
+function checkSpam(str){
+    let lowerwords = str.toLowerCase();
+    return lowerwords.includes ("1xBet") || lowerwords.includes ("XXX");
 }
